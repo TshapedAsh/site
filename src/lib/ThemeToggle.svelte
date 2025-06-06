c6da0b4143c56001da2106df7ec971334adc64db
Gaurav Gawade <156568590+TshapedAsh@users.noreply.github.com>
2025-06-07 04:21:14 +0530

    Create ThemeToggle.svelte

---
+++ src/lib/ThemeToggle.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import { theme, toggleTheme } from '$lib/stores/theme';
+</script>
+<button on:click={toggleTheme} class="p-2 rounded bg-gray-200 dark:bg-gray-700">
+  Toggle { $theme === 'light' ? 'Dark' : 'Light' }
+</button>
