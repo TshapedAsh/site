7af5a741e938320375130bae10665c42cadbb509
Gaurav Gawade <156568590+TshapedAsh@users.noreply.github.com>
2025-06-07 04:25:14 +0530

    Create +page.svelte

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  // blog page
+</script>
+<section class="p-8">
+  <h1 class="text-3xl font-semibold mb-4">Blog</h1>
+  <p class="text-gray-600 dark:text-gray-300">Coming soon...</p>
+</section>
