9232c0e237c16ae25654cd892993cd3210a22008
Gaurav Gawade <156568590+TshapedAsh@users.noreply.github.com>
2025-06-07 04:23:11 +0530

    Create +layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import '../app.css';
+  import { browser } from '$app/environment';
+  import { theme } from '$lib/stores/theme';
+  import ThemeToggle from '$lib/ThemeToggle.svelte';
+  $: if (browser) {
+    document.documentElement.className = $theme;
+  }
+</script>
+<ThemeToggle class="fixed top-4 right-4" />
+<slot />
